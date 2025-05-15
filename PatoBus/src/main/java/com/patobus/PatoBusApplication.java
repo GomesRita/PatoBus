697d698e3362cf7681729c6615725a68c5d2f5a8
Marcos Ribeiro <ribeiro.marcos@escolar.ifrn.edu.br>
2025-05-15 15:14:23 -0300

    Atualizando pasta do backend

---
+++ PatoBus/src/main/java/com/patobus/PatoBusApplication.java
@@ -0,0 +1,13 @@
+package com.patobus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatoBusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatoBusApplication.class, args);
+	}
+
+}
---
+++ PatoBus/src/test/java/com/patobus/PatoBusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.patobus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatoBusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
